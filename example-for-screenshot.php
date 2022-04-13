62c445ed52f720888b5e4e9e34917a9107615364
Dave Mills <dave-mills@users.noreply.github.com>
2022-04-13 14:13:05 +0100

    Add example code to generate screenshot image

---
+++ example-for-screenshot.php
@@ -0,0 +1,36 @@
+<?php
+
+protected function setupCreateOperation()
+{
+  
+  CRUD::addField([
+      'name' => 'field_name',
+      'type' => 'section-title',
+      'title' => 'Section Title without a content property',
+  ]);
+
+  CRUD::field('question 1');
+  CRUD::field('question 2');
+  CRUD::field('question 3');
+
+  CRUD::addField([
+      'name' => 'field_name2',
+      'type' => 'section-title',
+      'title' => 'Sub Heading',
+      'content' => 'Some information for the, to be displayed inside a callout box. This section-title has a title, content and divider = true.',
+      'variant' => 'info',
+      'divider' => true,
+  ]);
+
+  CRUD::field('question 1 for new section');
+  CRUD::field('question 2 for new section');
+
+  CRUD::addField([
+      'name' => 'warning',
+      'type' => 'section-title',
+      'variant' => 'danger',
+      'content' => 'NOTE: This example has no title property, and the variant = "danger" to highlight this very important information.',
+  ]);
+
+  CRUD::field('final question');
+}
